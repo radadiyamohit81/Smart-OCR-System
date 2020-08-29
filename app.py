ed0efcf0f7513156383aee9b7198a1ddc069af95
RadadiyaMohit <30775542+radadiyamohit81@users.noreply.github.com>
2020-08-29 22:17:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ except ImportError:
     import Image
 import pytesseract
 import os
+import shutil
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-Folder = '/uploads/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+app.config['TEMP_FOLDER'] = '/tmp'
 
 
 def allowed_file(filename):
@@ -26,14 +27,21 @@ def index():
 def upload():
     if request.method == 'POST':
         f = request.files['file']
-        if f.filename == '':
-            return render_template('index.html', msg='No file selected')
         if f and allowed_file(f.filename):
-            f.save(os.path.join(os.getcwd() + Folder, f.filename))
+            if not os.path.exists(os.path.join(app.config['TEMP_FOLDER'], str(os.getpid()))):
+                folder = os.path.join(app.config['TEMP_FOLDER'], str(os.getpid()))
+            else:
+                shutil.rmtree(os.path.join(app.config['TEMP_FOLDER'], str(os.getpid())))
+                # os.remove(os.path.join(app.config['TEMP_FOLDER'],str(os.getpid())))
+                folder = os.path.join(app.config['TEMP_FOLDER'], str(os.getpid()))
+            os.mkdir(folder)
+            input_file = os.path.join(folder, secure_filename(f.filename))
+            f.save(input_file)
             pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
-            text = pytesseract.image_to_string(Image.open(f))
+            text = pytesseract.image_to_string(input_file)
             return text
     return None
+
 
 if __name__ == '__main__':
     app.run(debug=True)
